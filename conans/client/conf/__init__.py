35792bfd458a11b0ce32e553e2c577592fe54268
Dan Williams <dnwillia@gmail.com>
2024-01-09 13:18:07 -0500

    Introduce default settings for gfortran

--- conans/client/conf/__init__.py
+++ conans/client/conf/__init__.py
@@ -99,6 +99,18 @@ compiler:
         threads: [null, posix, win32]  # Windows MinGW
         exception: [null, dwarf2, sjlj, seh]  # Windows MinGW
         cppstd: [null, 98, gnu98, 11, gnu11, 14, gnu14, 17, gnu17, 20, gnu20, 23, gnu23]
+    gfortran:
+        version: ["4.1", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "4.9.1",
+                    "5", "5.1", "5.2", "5.3", "5.4", "5.5",
+                    "6", "6.1", "6.2", "6.3", "6.4", "6.5",
+                    "7", "7.1", "7.2", "7.3", "7.4", "7.5",
+                    "8", "8.1", "8.2", "8.3", "8.4", "8.5",
+                    "9", "9.1", "9.2", "9.3", "9.4", "9.5",
+                    "10", "10.1", "10.2", "10.3", "10.4", "10.5"]
+        integer_size: [4, 8, 16]
+        real_size: [4, 8, 16]
+        underscoring: ["default", "none", "no_second"]
+        fortran_std: ["f95", "f2003", "f2008", "f2018", "f2023"]
     msvc:
         version: [170, 180, 190, 191, 192, 193]
         update: [null, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
